3c58627d63456845ae489e8302016412f2629b55
r123198 <tibudan.rafael@gmail.com>
2025-05-19 10:48:21 +0800

    changes for portfolio

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/rafael-2025' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/rafael-2025/' : '',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
